76ef2d63f7b36f9a69131c41f6a0410215f7fcce
szat <artabalt@gmail.com>
2018-03-28 16:23:42 -0400

    created SDF::build()

--- octree.h
+++ octree.h
@@ -7,15 +7,128 @@
 
 using namespace std;
 
+class SDF {
+private:
+	//Data
+	const vector<array<double, 3>> & V;
+	const vector<array<int, 3>> & F;
+	const vector<array<double, 3>> & bary;
+
+	//Node
+	array<array<double, 3>, 2> box;
+	vector<int> indices; //of triangles
+	array<unique_ptr<SDF>, 8> children;
+public:
+	SDF(const vector<array<double, 3>> & V, const vector<array<int, 3>> & F, const vector<array<double, 3>> & bary)
+		: V(V), F(F), bary(bary) {};
+	SDF(const SDF& other) = delete;
+	SDF& operator=(const SDF& rhs) = delete;
+
+	void init();
+	void build();
+};
+
+void SDF::init() {
+	for (size_t i = 0; i < this->F.size(); ++i) {
+		this->indices.push_back((int)i);
+	}
+}
+
+void SDF::build() {
+	if (this->indices.size() == 0) {
+		return;
+	}
+
+	//Compute box
+	array<double, 3> bound_down;
+	array<double, 3> bound_up;
+	double epsilon = 0.0000000001;
+	array<double, 3> first;
+	first = this->V.at(this->F.at(this->indices.at(0))[0]);
+	bound_up = { first[0] + epsilon, first[1] + epsilon, first[2] + epsilon };
+	bound_down = { first[0] - epsilon, first[1] - epsilon, first[2] - epsilon };
+
+	for (size_t i = 0; i < this->indices.size(); ++i) {
+		for (size_t j = 0; j < 3; ++j) {
+			array<double, 3> pt = this->V.at(this->F.at(this->indices.at(i))[j]);
+			for (size_t d = 0; d < 3; ++d) {
+				if (bound_up[d] < pt[d])
+					bound_up[d] = pt[d];
+				if (bound_down[d] > pt[d])
+					bound_down[d] = pt[d];
+			}
+		}
+	}
+	bound_up = { bound_up[0] + epsilon, bound_up[1] + epsilon, bound_up[2] + epsilon };
+	bound_down = { bound_down[0] - epsilon, bound_down[1] - epsilon, bound_down[2] - epsilon };
+	this->box = { bound_down, bound_up };
+
+	//More than one triangle remaining in the box
+	if (this->indices.size() > 1) {
+		//Compute center
+		array<double, 3> mean;
+		for (size_t i = 0; i < this->indices.size(); ++i) {
+			mean[0] += mean[0] + bary.at(this->indices.at(i))[0];
+			mean[1] += mean[1] + bary.at(this->indices.at(i))[1];
+			mean[2] += mean[2] + bary.at(this->indices.at(i))[2];
+		}
+		mean[0] = mean[0] / this->indices.size();
+		mean[1] = mean[1] / this->indices.size();
+		mean[2] = mean[2] / this->indices.size();
+
+		//Compute new indices
+		array<vector<int>, 8> sub_indices;
+
+		for (size_t i = 0; i < this->indices.size(); ++i) {
+			array<double, 3> pt = bary.at(indices.at(i));
+			if (pt[0] > mean[0]) {
+				if (pt[1] > mean[1]) {
+					if (pt[2] > mean[2])
+						sub_indices[0].push_back(indices.at(i));
+					else //pt[2] <= mean[2]
+						sub_indices[1].push_back(indices.at(i));
+				}
+				else {   //pt[1] <= mean[1]
+					if (pt[2] > mean[2])
+						sub_indices[2].push_back(indices.at(i));
+					else //pt[2] <= mean[2]
+						sub_indices[3].push_back(indices.at(i));
+				}
+			}
+			else {		 //pt[0] <= mean[0]
+				if (pt[1] > mean[1]) {
+					if (pt[2] > mean[2])
+						sub_indices[4].push_back(indices.at(i));
+					else //pt[2] <= mean[2]
+						sub_indices[5].push_back(indices.at(i));
+				}
+				else {   //pt[1] <= mean[1]
+					if (pt[2] > mean[2])
+						sub_indices[6].push_back(indices.at(i));
+					else //pt[2] <= mean[2]
+						sub_indices[7].push_back(indices.at(i));
+				}
+			}
+		}
+
+		//Create children and recurse
+		for (size_t i = 0; i < 8; ++i) {
+			if (sub_indices[i].size() > 0) {
+				this->children[i] = unique_ptr<SDF>(new SDF(this->V, this->F, this->bary));
+				this->children[i]->indices = sub_indices[i];
+				this->children[i]->build();
+			}
+		}
+	}
+	return;
+}
+
 double dot(const array<double, 3> &A, const array<double, 3> &B) {
 	double out = A[0] * B[0] + A[1] * B[1] + A[2] * B[2];
 	return out;
 }
 
 array<double, 3> cross(const array<double, 3> &A, const array<double, 3> &B) {
-	//a = A.y * B.z - A.z * B.y;
-	//b = A.z * B.x - A.x * B.z;
-	//c = A.x * B.y - A.y * B.x;
 	array<double, 3> out;
 	out[0] = A[1] * B[2] - A[2] * B[1];
 	out[1] = A[2] * B[0] - A[0] * B[2];
@@ -23,30 +136,30 @@ array<double, 3> cross(const array<double, 3> &A, const array<double, 3> &B) {
 	return out;
 }
 
-bool halfRayTriangleIntersect(const array<double,3> & source, const array<double,3> & dir, const array<double,9> & tri, array<double,3> & intersection) {
+bool RayTriangle(const array<double, 3> & source, const array<double, 3> & dir, const array<array<double, 3>, 3> & tri, array<double, 3> & intersection) {
 	//https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
-	//This function returns a coordinate in 3d 
-	//A vector of points in 3d
-	const double epsilon = 0.0000001;
-	array<double, 3> v0 = { tri[0], tri[1], tri[2] };
-	array<double, 3> v1 = { tri[3], tri[4], tri[5] };
-	array<double, 3> v2 = { tri[6], tri[7], tri[8] }; 
+	const double epsilon = 0.00000001;
+	array<double, 3> v0 = tri[0];
+	array<double, 3> v1 = tri[1];
+	array<double, 3> v2 = tri[2];
 	array<double, 3> h, s, q;
 	double a, f, u, v;
 	array<double, 3> edge1 = { v1[0] - v0[0], v1[1] - v0[1] , v1[2] - v0[2] };
 	array<double, 3> edge2 = { v2[0] - v0[0], v2[1] - v0[1] , v2[2] - v0[2] };
 	h = cross(dir, edge2);
 	a = dot(edge1, h);
-	if (a > -epsilon && a < epsilon)
+	if (a > -epsilon && a < epsilon) {
+		//DOES NOT HANDLE CASE WHEN LINE IS PARALLEL TO TRIANGLE PLANE
 		return false;
+	}
 	f = 1 / a;
 	s = { source[0] - v0[0], source[1] - v0[1], source[2] - v0[2] };
 	u = f * dot(s, h);
-	if (u < 0.0 || u > 1.0)
+	if (u < 0.0 - epsilon || u > 1.0 + epsilon)
 		return false;
 	q = cross(s, edge1);
 	v = f * dot(dir, q);
-	if (v < 0.0 || u + v > 1.0)
+	if (v < 0.0 - epsilon || u + v > 1.0 + epsilon)
 		return false;
 	// At this stage we can compute t to find out where the intersection point is on the line.
 	double t = f * dot(edge2, q);
@@ -60,18 +173,17 @@ bool halfRayTriangleIntersect(const array<double,3> & source, const array<double
 		return false;
 }
 
-bool halfRayBoxIntersect(const array<double,3> & source, const array<double,3> & dir, double low_t, double high_t, const array<array<double,3>,2> & box) {
+bool RayBox(const array<double, 3> & source, const array<double, 3> & dir, double low_t, double high_t, const array<array<double, 3>, 2> & box) {
 	//https://www.cs.utah.edu/~awilliam/box/box.pdf
 	array<double, 3> inv_dir = { 1 / dir[0], 1 / dir[1], 1 / dir[2] };
 	array<int, 3> sign;
 	sign[0] = inv_dir[0] < 0;
 	sign[1] = inv_dir[1] < 0;
 	sign[2] = inv_dir[2] < 0;
-
 	double tmin, tmax, tymin, tymax, tzmin, tzmax;
-	tmin =  (box[sign[0]][0] -   source[0]) * inv_dir[0];
-	tmax =  (box[1-sign[0]][0] - source[0]) * inv_dir[0];
-	tymin = (box[sign[1]][1] -   source[1]) * inv_dir[1];
+	tmin = (box[sign[0]][0] - source[0]) * inv_dir[0];
+	tmax = (box[1 - sign[0]][0] - source[0]) * inv_dir[0];
+	tymin = (box[sign[1]][1] - source[1]) * inv_dir[1];
 	tymax = (box[1 - sign[1]][1] - source[1]) * inv_dir[1];
 	if ((tmin > tymax) || (tymin > tmax))
 		return false;
@@ -79,8 +191,8 @@ bool halfRayBoxIntersect(const array<double,3> & source, const array<double,3> &
 		tmin = tymin;
 	if (tymax < tmax)
 		tmax = tymax;
-	tzmin = (box[sign[2]][2] -   source[2]) * inv_dir[2];
-	tzmax = (box[1-sign[2]][2] - source[2]) * inv_dir[2];
+	tzmin = (box[sign[2]][2] - source[2]) * inv_dir[2];
+	tzmax = (box[1 - sign[2]][2] - source[2]) * inv_dir[2];
 	if ((tmin > tzmax) || (tzmin > tmax))
 		return false;
 	if (tzmin > tmin)
@@ -88,6 +200,4 @@ bool halfRayBoxIntersect(const array<double,3> & source, const array<double,3> &
 	if (tzmax < tmax)
 		tmax = tzmax;
 	return ((tmin < high_t) && (tmax > low_t));
-
-	return true;
 }
---
+++ testing.h
@@ -0,0 +1,192 @@
+#pragma once
+#include <random>
+#include <chrono>
+#include <ctime>
+#include "octree.h"
+
+using namespace std;
+
+void test_RayBox() {
+	cout << "Int test RayBox ..." << endl;
+	double epsilon = 0.00000001;
+	array<array < double, 3>, 2> box;
+	box[0] = { 0,0,0 };
+	box[1] = { 2,2,2 };
+	array<double, 3> source;
+	array<double, 3> dir;
+	double low_t = 0;
+	double high_t = DBL_MAX;
+
+	//Test no intersection
+	source = { 1,1,1 };
+	dir = { 1,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] <<"," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 0,0,0 };
+	dir = { 1,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { -1,-1,0 };
+	dir = { 1,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 1,0,0 };
+	dir = { 1,0,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 0,1,0 };
+	dir = { 0,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 0,0,1 };
+	dir = { 0,0,1 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 2,2,2 };
+	dir = { 1,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { -2,0,0 };
+	dir = { 1,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	box[0] = { box[0][0] - epsilon, box[0][1] - epsilon, box[0][2] - epsilon };
+	box[1] = { box[1][0] + epsilon, box[1][1] + epsilon, box[1][2] + epsilon };
+
+	source = { 0,1,0 };
+	dir = { 0,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 0,0,1 };
+	dir = { 0,0,1 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 2,2,2 };
+	dir = { 1,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { -2,0,0 };
+	dir = { 1,1,0 };
+	cout << "Box = [" << box[0][0] << "," << box[0][1] << "," << box[0][2] << "],[" << box[1][0] << "," << box[1][1] << "," << box[1][2] << "]; ";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayBox(source, dir, low_t, high_t, box)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(-1000, 1000);
+	double total = 0;
+	for (int i = 0; i < 1000; ++i) {
+		box[0] = { (double)dis(gen), (double)dis(gen), (double)dis(gen) };
+		box[1] = { (double)dis(gen), (double)dis(gen), (double)dis(gen) };
+		source = { (double)dis(gen), (double)dis(gen), (double)dis(gen) };
+		dir =    { (double)dis(gen), (double)dis(gen), (double)dis(gen) };
+		chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+		RayBox(source, dir, low_t, high_t, box);
+		chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+		chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+		total += time_span.count();
+	}
+	total /= 1000;
+	cout << "Average running time for RayBox is " << total << " seconds." << endl;
+}
+
+void test_RayTriangle() {
+	cout << "Int test RayBox ..." << endl;
+	double epsilon = 0.00000001;
+	array<array < double, 3>, 3> tri;
+	tri[0] = { 0,0,0 };
+	tri[1] = { 3,0,0 };
+	tri[2] = { 0,3,0 };
+
+	array<double, 3> source;
+	array<double, 3> dir;
+	array<double, 3> intersect;
+
+	//Test
+	source = { 0,0,0 };
+	dir = { 1,0,0 };
+	cout << "Tri = [" << tri[0][0] << "," << tri[0][1] << "," << tri[0][2] << "],[" << tri[1][0] << "," << tri[1][1] << "," << tri[1][2] << "],[" << tri[2][0] << "," << tri[2][1] << "," << tri[2][2] << "]";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayTriangle(source, dir, tri, intersect)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { -1,0,0 };
+	dir = { 1,0,0 };
+	cout << "Tri = [" << tri[0][0] << "," << tri[0][1] << "," << tri[0][2] << "],[" << tri[1][0] << "," << tri[1][1] << "," << tri[1][2] << "],[" << tri[2][0] << "," << tri[2][1] << "," << tri[2][2] << "]";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayTriangle(source, dir, tri, intersect)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 0,0,0 };
+	dir = { 1,1,0 };
+	cout << "Tri = [" << tri[0][0] << "," << tri[0][1] << "," << tri[0][2] << "],[" << tri[1][0] << "," << tri[1][1] << "," << tri[1][2] << "],[" << tri[2][0] << "," << tri[2][1] << "," << tri[2][2] << "]";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayTriangle(source, dir, tri, intersect)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { -1,0,0 };
+	dir = { -1,-1,0 };
+	cout << "Tri = [" << tri[0][0] << "," << tri[0][1] << "," << tri[0][2] << "],[" << tri[1][0] << "," << tri[1][1] << "," << tri[1][2] << "],[" << tri[2][0] << "," << tri[2][1] << "," << tri[2][2] << "]";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayTriangle(source, dir, tri, intersect)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 1,1,1 };
+	dir = { 0,0,-1 };
+	cout << "Tri = [" << tri[0][0] << "," << tri[0][1] << "," << tri[0][2] << "],[" << tri[1][0] << "," << tri[1][1] << "," << tri[1][2] << "],[" << tri[2][0] << "," << tri[2][1] << "," << tri[2][2] << "]";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayTriangle(source, dir, tri, intersect)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { 1,1,0 };
+	dir = { 0,0,1 };
+	cout << "Tri = [" << tri[0][0] << "," << tri[0][1] << "," << tri[0][2] << "],[" << tri[1][0] << "," << tri[1][1] << "," << tri[1][2] << "],[" << tri[2][0] << "," << tri[2][1] << "," << tri[2][2] << "]";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayTriangle(source, dir, tri, intersect)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+	source = { -1,-1,0 };
+	dir = { 0,0,1 };
+	cout << "Tri = [" << tri[0][0] << "," << tri[0][1] << "," << tri[0][2] << "],[" << tri[1][0] << "," << tri[1][1] << "," << tri[1][2] << "],[" << tri[2][0] << "," << tri[2][1] << "," << tri[2][2] << "]";
+	cout << "Ray, source = [" << source[0] << "," << source[1] << "," << source[2] << "], dir = [" << dir[0] << "," << dir[1] << "," << dir[2] << "]; ";
+	if (true == RayTriangle(source, dir, tri, intersect)) cout << "RayBox(source, dir, box) == true" << endl;
+	else cout << "RayBox(source, dir, box) == false" << endl;
+
+
+}
+
+
